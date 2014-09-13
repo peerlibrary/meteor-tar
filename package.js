ab90eecd10e59a7cdbbcac084471d76fb7774f71
Mitar <mitar.git@tnode.com>
2014-09-13 10:18:54 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,26 +1,10 @@
 Package.describe({
   summary: "Meteor smart package for tar node.js package",
   version: '0.1.20-3',
-  name: 'peerlibrary:tar',
+  name: 'mrt:tar',
   git: 'https://github.com/peerlibrary/meteor-tar.git'
 });
 
-Npm.depends({
-  tar: '0.1.20'
-});
-
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-
-  api.export('tar');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:tar', 'tinytest', 'test-helpers'], 'server');
-
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:tar@0.1.20-3');
 });
