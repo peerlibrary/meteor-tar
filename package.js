2df48c34f2b1a0ab2a00657b5a772838e7f5f57d
Mitar <mitar.git@tnode.com>
2015-01-17 08:22:22 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   summary: "Meteor smart package for tar node.js package",
-  version: '0.1.20_4',
+  version: '1.0.3_1',
   name: 'peerlibrary:tar',
   git: 'https://github.com/peerlibrary/meteor-tar.git'
 });
 
 Npm.depends({
-  tar: '0.1.20'
+  tar: '1.0.3'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.3');
+  api.versionsFrom('METEOR@1.0.2.1');
 
   api.export('tar');
 
